0084d5acf76c4bbee8fab3b70990680a4ed9e5f5
nopeless <syul0608@naver.com>
2021-01-19 07:09:07 -0800

    initial

---
+++ src/components/DiscordEmbed.vue
@@ -0,0 +1,22 @@
+<template>
+  <iframe
+    src="https://discord.com/widget?id=493173110799859713&theme=dark"
+    width="100%"
+    height="500"
+    allowtransparency="true"
+    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: "discord widget"
+};
+</script>
+
+<style lang="scss" scoped>
+iframe {
+  // discord is mustarded and has this weird white corners. this important style remvoes that.
+  background: transparent !important;
+}
+</style>
