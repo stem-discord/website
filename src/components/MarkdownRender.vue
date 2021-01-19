ae0729f72b2ab910e336472f66b9e264f44119c7
nopeless <syul0608@naver.com>
2021-01-19 08:22:50 -0800

    initial

---
+++ src/components/MarkdownRender.vue
@@ -0,0 +1,27 @@
+/* eslint-disable */
+<template>
+  <h1>the url is {{ url }}, content is {{ content }}</h1>
+  <div v-html="compiledMarkdown"></div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+const content = ref("");
+
+fetch(/*fk this for now*/).then(r => r.text().then(t => (content.value = t)));
+
+export default {
+  setup() {
+    const md = ref("");
+    return {
+      md,
+      content
+    };
+  },
+  name: "markdown renderer",
+  props: {
+    url: String
+  }
+};
+</script>
