c417c9f6f9ca3fcc017cf6e0733f2bee09dbdbaf
nopeless <syul0608@naver.com>
2021-01-19 08:24:15 -0800

    init

---
+++ src/views/MarkdownTest.vue
@@ -0,0 +1,15 @@
+<template>
+  <MarkdownRender
+    url="https://raw.githubusercontent.com/stem-discord/public/main/STEM_Rules.md"
+  />
+</template>
+
+<script>
+import MarkdownRender from "@/components/MarkdownRender.vue";
+
+export default {
+  components: {
+    MarkdownRender
+  }
+};
+</script>
