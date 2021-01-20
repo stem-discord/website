16a3491bb3f26ad1f435eb729aa6965b16e4defa
nopeless <nopeless@users.noreply.github.com>
2021-01-20 04:55:03 -0800

    experimental edits

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Thonk land" />
+    <RP :text="'hii'" times="3" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import RP from "@/components/RepeatString.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    RP
   }
 };
 </script>
