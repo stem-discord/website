eeb4a13477b39ddf055296c7b93e098eb1c31217
nopeless <nopeless@users.noreply.github.com>
2021-01-25 08:33:55 -0800

    cleanup

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const fs = require("fs");
 require("dotenv").config({ path: __dirname + "/.env" });
 
 const http = require("http");
-const https = require("https");
-
-const fs = require("fs");
-const path = require("path");
-const morgan = require("morgan");
-
-let certInfos = {};
+let certinfos;
 if (!process.env.NO_HTTPS) {
-  console.log("key, cert files will not be loaded");
-  certInfos = {
+  const https = require("https");
+   certInfos = {
     key: fs.readFileSync(process.env.KEY || __dirname + "/local/key.pem"),
     cert: fs.readFileSync(process.env.CERT || __dirname + "/local/cert.pem")
   };
+  console.log("key, cert files loaded");
+} else {
+  console.log("running on http");
+  
 }
+
+const path = require("path");
+const morgan = require("morgan");
+
 const app = express();
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(
@@ -34,16 +37,7 @@ app.use(
   })
 );
 
-let httpRerouteApp = express();
-if (!process.env.NO_HTTPS) {
-  console.log("disabling https redirect");
-  httpRerouteApp.get("*", (req, res) => {
-    res.redirect("https://" + req.headers.host + req.url);
-  });
-}
 
-var httpServer = http.createServer(httpRerouteApp);
-var httpsServer = https.createServer(certInfos, app);
 
 app.use(express.static("html"));
 // fall back page
@@ -51,7 +45,27 @@ app.use((req, res) => {
   res.render("test");
 });
 
-console.log(`http port: ${process.env.HTTP_PORT || 80}\nhttps port: ${process.env.HTTPS_PORT || 443}`);
-console.log(`click here to connect http://${process.env.HOST || "localhost"}:${process.env.HTTP_PORT}`);
+// if http mode, make this the main server
+let httpServer;
+if (!process.env.NO_HTTPS) {
+  let httpRerouteApp = express();
+  httpServer = http.createServer(httpRerouteApp);
+  httpRerouteApp.get("*", (req, res) => {
+    res.redirect("https://" + req.headers.host + req.url);
+  });
+} else {
+  httpServer = http.createServer(app);
+}
+
+console.log(`http port: ${process.env.HTTP_PORT || 80}`);
+if (!process.env.NO_HTTPS) console.log(`https port: ${process.env.HTTPS_PORT || 443}`);
+console.log(`click here to connect http://${process.env.HOST || "localhost"}:${process.env.HTTP_PORT || 80}`);
+
 httpServer.listen(process.env.HTTP_PORT || 80);
-httpsServer.listen(process.env.HTTPS_PORT || 443);
+
+if (!process.env.NO_HTTPS) {
+  let httpsServer = https.createServer(certInfos, app);
+  httpsServer.listen(process.env.HTTPS_PORT || 443);
+} else {
+  console.log("https redirect disabled");
+}
