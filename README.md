4532d66b0fe8f91f48b2e1d5881d6c6ecfcf4392
nopeless <nopeless@users.noreply.github.com>
2021-01-25 08:14:33 -0800

    added env options

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 # testing
 
-`./local/` requires `key.pem` and `cert.pem`
+`./local/` requires `key.pem` and `cert.pem` (unencrypted)
+
+`NO_HTTPS` - allows an http only server
+
+`KEY` - key directory (defaults to `./local/key.pem`)
+
+`CERT` - cert directory (defaults to `./local/cert.pem`)
 
 run `npm run server`
--- app.js
+++ app.js
@@ -8,11 +8,14 @@ const fs = require("fs");
 const path = require("path");
 const morgan = require("morgan");
 
-var certInfos = {};
-certInfos = {
-  key: fs.readFileSync(__dirname + "/local/key.pem"),
-  cert: fs.readFileSync(__dirname + "/local/cert.pem"),
-};
+let certInfos = {};
+if (!process.env.NO_HTTPS) {
+  console.log("key, cert files will not be loaded");
+  certInfos = {
+    key: fs.readFileSync(process.env.KEY || __dirname + "/local/key.pem"),
+    cert: fs.readFileSync(process.env.CERT || __dirname + "/local/cert.pem")
+  };
+}
 const app = express();
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(
@@ -27,19 +30,23 @@ app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 app.use(
   express.urlencoded({
-    extended: true,
+    extended: true
   })
 );
 
 let httpRerouteApp = express();
-httpRerouteApp.get("*", (req, res) => {
-  res.redirect("https://" + req.headers.host + req.url);
-});
+if (!process.env.NO_HTTPS) {
+  console.log("disabling https redirect");
+  httpRerouteApp.get("*", (req, res) => {
+    res.redirect("https://" + req.headers.host + req.url);
+  });
+}
 
 var httpServer = http.createServer(httpRerouteApp);
 var httpsServer = https.createServer(certInfos, app);
 
 app.use(express.static("html"));
+// fall back page
 app.use((req, res) => {
   res.render("test");
 });
