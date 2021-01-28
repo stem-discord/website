7454ca7df01445dedd36c23a56aa3cd5b1412ea7
nopeless <38830903+nopeless@users.noreply.github.com>
2021-01-29 04:41:55 +0900

    added express backend

--- srv/index.js
+++ srv/index.js
@@ -1,13 +1,25 @@
 import express from "express";
 // import socketIO from "socket.io";
 
-export default (app, http) => {
-  // app.use(express.json());
-  //
-  // app.get('/foo', (req, res) => {
-  //   res.json({msg: 'foo'});
-  // });
-  //
+
+module.exports = (app) => {
+  // api
+  app.use(express.json());
+  app.get(`/api`, (req, res) => {
+    res.json({msg: `works`});
+  });
+
+  // vue view
+  app.use(require(`connect-history-api-fallback`)());
+
+  // // vue output
+  // app.set(`views`, `dist`);
+  // app.set(`view engine`, `html`);
+  
+  // TODO: i was originally going to use views instead of static but for some reason i cant get views to work and the below works just fine idk maybe ill fix it when im better at express or smth
+  app.use(express.static(`dist`));
+
+  
   // app.post('/bar', (req, res) => {
   //   res.json(req.body);
   // });
