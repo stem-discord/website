9c3f547cf17b9649946e46814176f1d764a453ab
nopeless <38830903+nopeless@users.noreply.github.com>
2021-01-29 05:24:46 +0900

    added logger

--- srv/index.js
+++ srv/index.js
@@ -1,13 +1,27 @@
-import express from "express";
+const express = require(`express`);
+const path = require(`path`);
+const morgan = require(`morgan`);
+const fs = require(`fs`);
 // import socketIO from "socket.io";
 
-
+// https redirect is done on nginx
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, `access.log`),
+  { flags: `a` },
+);
 module.exports = (app) => {
+  // logs
+  app.use(morgan(`combined`, { stream: accessLogStream }));
+
   // api
   app.use(express.json());
   app.get(`/api`, (req, res) => {
     res.json({msg: `works`});
   });
+
+  app.get(`/login`, (req, res) => {
+    res.json({msg: `l`});
+  }); 
 
   // vue view
   app.use(require(`connect-history-api-fallback`)());
