552bf930802a1f4c3e651ef4cc0134417c2c44b2
nopeless <38830903+nopeless@users.noreply.github.com>
2021-01-29 21:36:38 +0900

    export bridge

---
+++ srv/db/index.js
@@ -0,0 +1,9 @@
+const UserSchema = require(`${__dirname}/models/User.js`);
+const CookieSchema = require(`${__dirname}/models/Cookie.js`);
+const MemeSchema = require(`${__dirname}/models/Meme.js`);
+
+module.exports = {
+  UserSchema,
+  CookieSchema,
+  MemeSchema,
+};
