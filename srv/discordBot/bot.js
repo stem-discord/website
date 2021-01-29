56c94549eb972a7a23bb20384d59fa302bb035ef
nopeless <38830903+nopeless@users.noreply.github.com>
2021-01-29 21:34:07 +0900

    frame

---
+++ srv/discordBot/bot.js
@@ -0,0 +1,29 @@
+// integrated with the backend itself
+const Discord = require(`discord.js`);
+const { Client } = Discord;
+require(`dotenv`).config({ path: `../.env` });
+
+function sleep(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+const client = new Client({
+  partials: [`MESSAGE`, `CHANNEL`, `REACTION`],
+  disableMentions: `all`,
+});
+
+const pingEveryoneRoleOption = {
+  disableMentions: `none`,
+};
+
+client.on(`message`, async message => {
+	if ()
+});
+
+function uploadFile() {
+
+}
+
+module.exports = {
+  uploadFile,
+};
