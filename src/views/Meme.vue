249697588ce5649a9e47b599c43158aa9f273976
Karunika <karunikatrip@gmail.com>
2021-01-30 19:25:59 +0100

    Why not use nesting when using Scss lol

--- src/views/Meme.vue
+++ src/views/Meme.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="form-wrapper">
-    <form class="meme-form">
+    <form>
       <h1>Meme Form</h1>
-      <span class="meme-title-input">
+      <span>
         <input type="text" 
                name="meme-title"
                required />
@@ -36,57 +36,60 @@ export default {
     display: flex;
     align-content: center;
     justify-content: center;
-    .meme-form h1{
-      font-size: 3rem;
+    form{
+      display: flex;
+      flex-direction: column;
+      align-items: stretch;
+      justify-content: space-between;
+      padding: 2rem;
+      background: #00000000;
+      border-radius: 2rem;
+      box-shadow: 0 0 2rem #00000041;
+
+      h1{
+        font-size: 3rem;
+      }
+      span{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        margin: 1rem 0;
+        width: 100%;
+
+        input{
+          width: 100%;
+          padding: 0.8rem;
+          background: none;
+          border-bottom: solid 2px #613a94a1;
+          margin-bottom: 1rem;
+        }
+        label{
+          position: absolute;
+          margin-left: 1.2rem;
+          transition: all 0.4s ease;
+        }
+        input:valid ~ label, input:focus ~ label{
+          font-size: 1.2rem;
+          transform: translate(-0.2rem, -3rem);
+          color: rgb(116, 116, 116);
+        }
+        input:focus{
+          border-bottom: solid 2px var(--main-alpha);
+        }
+      }
+      button{
+        padding: 0.6rem;
+        margin: 2.6rem 0 0.2rem 0;
+        border-radius: 0.4rem;
+        color: white;
+        background-color: var(--main-alpha);
+        cursor: pointer;
+        transition: all 0.4s ease;
+      }
+      button:hover{
+        box-shadow: 0 0 1rem #00000041;
+      }
     }
   }
-  .meme-form{
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: space-between;
-    padding: 2rem;
-    background: #00000000;
-    border-radius: 2rem;
-    box-shadow: 0 0 2rem #00000041;
-  }
-  .meme-title-input{
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    margin: 1rem 0;
-    width: 100%;
-  }
-  .meme-title-input input{
-    width: 100%;
-    padding: 0.8rem;
-    background: none;
-    border-bottom: solid 2px #613a94a1;
-    margin-bottom: 1rem;
-  }
-  .meme-title-input label{
-    position: absolute;
-    margin-left: 1.2rem;
-    transition: all 0.4s ease;
-  }
-  .meme-title-input input:valid ~ label, .meme-title-input input:focus ~ label{
-    font-size: 1.2rem;
-    transform: translate(-0.2rem, -3rem);
-    color: rgb(116, 116, 116);
-  }
-  .meme-title-input input:focus{
-    border-bottom: solid 2px var(--main-alpha);
-  }
-  .meme-form button{
-    padding: 0.6rem;
-    margin: 2.6rem 0 0.2rem 0;
-    border-radius: 0.4rem;
-    color: white;
-    background-color: var(--main-alpha);
-    cursor: pointer;
-    transition: all 0.4s ease;
-  }
-  .meme-form button:hover{
-    box-shadow: 0 0 1rem #00000041;
-  }
+  
 </style>
