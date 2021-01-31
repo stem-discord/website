724209449fc055c9bd6780010d787629e21a707b
Rick Astley <54081925+rickastley1@users.noreply.github.com>
2021-01-31 13:44:42 +0000

    Added login button to top navbar
    I did it smetch

--- src/components/NavSection.vue
+++ src/components/NavSection.vue
@@ -6,6 +6,14 @@
     <div>
       <h1>STEM Helpers & Students</h1>
     </div>
+
+    <button id="login">
+      <div id="login-button">
+        <img id="avatar" 
+             src="../assets/avatar.png"/>
+        <p>Log in</p>
+      </div>
+    </button>
   </div>
 </template>
 
@@ -31,5 +39,27 @@ img {
   width: auto;
   object-fit: contain;
   max-width: 100%;
+}
+#login-button {
+  background-color: #fbe9e6;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 5px 10px;
+}
+#login {
+  width: 200px;
+  margin-left: auto;
+  margin-right: 10px;
+}
+#login-button:hover {
+  background-color: #e9b3e2;
+  transition-duration: 250ms;
+}
+#avatar {
+  border-radius: 50%;
+  background-color: black;
+  margin-right: 10px;
 }
 </style>
