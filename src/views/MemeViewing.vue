8b2621ca8d201d2829a2e8b62689fe8bf4fb769d
Rick Astley <54081925+rickastley1@users.noreply.github.com>
2021-02-01 19:52:43 +0000

    added the thing where you see memes

---
+++ src/views/MemeViewing.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-for="meme in memes" 
+       :key="meme">
+    <div class="meme-image-wrapper">
+      <img :src="meme.url"
+           class="meme-image"/>
+    </div>
+    <div class="meme-info">
+      <p class="meme-maker">{{ meme.ownerId }}</p>
+      <p class="meme-date">{{ meme.comments[0].timestamp }}</p>
+    </div>
+    <div class="updownvote">
+      <button class="upvote">
+        &uarr;
+      </button>
+      <button class="downvote">
+        &darr;
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style lang="scss" scoped>
+    div {
+        height: 500px;
+        background-color: red;
+    }
+</style>
