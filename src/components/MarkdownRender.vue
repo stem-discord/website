2b30349f28e65e071c2dfed13a91096556aae07e
Karunika <karunikatrip@gmail.com>
2021-02-02 15:02:39 +0100

    Converted into ES6 syntax, cause... why not?

--- src/components/MarkdownRender.vue
+++ src/components/MarkdownRender.vue
@@ -8,13 +8,53 @@
   </div>
 </template>
 
+<script>
+export default {
+  mounted() {
+    // console.log(this.$refs);
+    const o = this.$refs.markdownUrl;
+    const url = o.textContent;
+    o.textContent = `downloading...`;
+    (async function fetchMarkdownText(){
+      const r = await fetch(url);
+      const text = await r.text();
+      const res = await fetch(`https://api.github.com/markdown`, {
+        headers: {
+          'Content-Type': `application/json`,
+        },
+        method: `POST`,
+        body: JSON.stringify({
+          accept: `application/vnd.github.v3+json`,
+          mode: `markdown`,
+          text,
+        }),
+      });
+      const parsedText = await res.text();
+      o.innerHTML = parsedText;
+    })();
+    // fetch(url).then(async r => {
+    //   // actually set the parsed md
+    //   const text = await r.text();
+    //   const res = await fetch(`https://api.github.com/markdown`, {
+    //     headers: {
+    //       'Content-Type': `application/json`,
+    //     },
+    //     method: `POST`,
+    //     body: JSON.stringify({
+    //       accept: `application/vnd.github.v3+json`,
+    //       mode: `markdown`,
+    //       text,
+    //     }),
+    //   }).then(r => r.text());
+    //   o.innerHTML = res;
+    // },
+    // );
+  },
+};
+
+</script>
+
 <style>
-
-  * {
-    padding: 15px;
-    font-size: medium;
-  }
-
 	.markdown-body {
 		box-sizing: border-box;
 		min-width: 200px;
@@ -42,31 +82,3 @@
      font-size: medium; 
   }
 </style>
-<script>
-export default {
-  mounted() {
-    // console.log(this.$refs);
-    const o = this.$refs.markdownUrl;
-    const url = o.textContent;
-    o.textContent = `downloading...`;
-    fetch(url).then(async r => {
-      // actually set the parsed md
-      const text = await r.text();
-      const res = await fetch(`https://api.github.com/markdown`, {
-        headers: {
-          'Content-Type': `application/json`,
-        },
-        method: `POST`,
-        body: JSON.stringify({
-          accept: `application/vnd.github.v3+json`,
-          mode: `markdown`,
-          text,
-        }),
-      }).then(r => r.text());
-      o.innerHTML = res;
-    },
-    );
-  },
-};
-
-</script>
