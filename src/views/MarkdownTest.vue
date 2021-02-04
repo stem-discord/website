8b4c946ef3ab0cc7520624ea4d1ca0f0533b45c0
nopeless <38830903+nopeless@users.noreply.github.com>
2021-02-05 05:02:11 +0900

    added reload button

--- src/views/MarkdownTest.vue
+++ src/views/MarkdownTest.vue
@@ -1,15 +1,36 @@
 <template>
-  <MarkdownRender>
+  <button @click="updateContent">last updated: {{ lastUpdated }}</button>
+  <MarkdownRender ref="md"
+                  @updateTimestamp="updateTimestamp">
     https://raw.githubusercontent.com/stem-discord/public/main/STEM_Rules.md
   </MarkdownRender>
 </template>
-
+// ? `| last update: ${lastUpdated} ago` : ""
 <script>
 import MarkdownRender from "@/components/MarkdownRender.vue";
+import { ref } from "vue";
+
 
 export default {
   components: {
     MarkdownRender,
+  },
+  methods: {
+    updateContent() {
+      // console.log(`trying to update content`);
+      this.$refs.md.update();
+    },
+    updateTimestamp({ timestampString }) {
+      this.lastUpdated = timestampString.value;
+    },
+  },
+  setup() {
+    let lastUpdated = ref(`loading...`);
+    return {
+      lastUpdated,
+    };
+  },
+  mounted() {
   },
 };
 </script>
