2212ec5029e219b6ecd44c620cf5ab0be77ca46b
Karunika <karunikatrip@gmail.com>
2021-02-05 18:11:37 +0100

    Last updated

--- src/views/MarkdownTest.vue
+++ src/views/MarkdownTest.vue
@@ -1,14 +1,15 @@
 <template>
-  <button @click="updateContent">last updated: {{ lastUpdated }}</button>
   <MarkdownRender ref="md"
                   @updateTimestamp="updateTimestamp">
     https://raw.githubusercontent.com/stem-discord/public/main/STEM_Rules.md
   </MarkdownRender>
+  <button @click="updateContent">Last updated: {{ lastUpdated }}</button>
 </template>
 // ? `| last update: ${lastUpdated} ago` : ""
 <script>
 import MarkdownRender from "@/components/MarkdownRender.vue";
 import { ref } from "vue";
+import moment from "moment";
 
 
 export default {
@@ -20,8 +21,9 @@ export default {
       // console.log(`trying to update content`);
       this.$refs.md.update();
     },
-    updateTimestamp({ timestampString }) {
-      this.lastUpdated = timestampString.value;
+    updateTimestamp({ timestampString: { value } }) {
+      this.lastUpdated = `${moment(String(value)).calendar()}
+      (${moment(String(value)).fromNow()})`;
     },
   },
   setup() {
@@ -34,3 +36,24 @@ export default {
   },
 };
 </script>
+<style scoped>
+button{
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 6rem;
+  position: relative;
+  display: block;
+  max-width: 980px;
+  min-width: 200px;
+  background-color: rgba(0,0,0,0);
+  color: var(--main-alpha);
+  border-radius: 1rem;
+  text-align: left;
+  opacity: 0.7;
+  cursor: pointer;
+  transition: all 0.4s ease;
+}
+button:hover{
+  opacity: 1;
+}
+</style>
