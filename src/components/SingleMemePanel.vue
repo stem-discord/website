02add65145495fd36fb98164a901f940f9742380
nopeless <nopeless@users.noreply.github.com>
2021-02-12 11:47:49 -0800

    meme working proto 1

---
+++ src/components/SingleMemePanel.vue
@@ -0,0 +1,18 @@
+// this should be a single meme with an upvote, downvote, and report button
+<template>
+  <div class="meme-panel">
+    meme
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style>
+div.meme-panel {
+  background-color: black;
+}
+</style>
