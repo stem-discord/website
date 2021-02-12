1f43bcfefb4fa76e48874dc85efc69b80833f351
nopeless <nopeless@users.noreply.github.com>
2021-02-12 13:39:12 -0800

    someone fix this if u can

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,19 @@ const fs = require(`fs`);
 
 module.exports = {
   chainWebpack: config => {
+    // https://github.com/vuejs/vue-next/issues/1414#issuecomment-648998826
+    // this shit doesnt work
+    // config.module
+    //   .rule(`vue`)
+    //   .use(`vue-loader`)
+    //   .loader(`vue-loader`)
+    //   .tap(options => {
+    //     options.compilerOptions = {
+    //       ...options.compilerOptions,
+    //       isCustomElement: tag => /^ion-/.test(tag),
+    //     };
+    //     return options;
+    //   });
     config.plugin(`html`).tap(args => {
       args[0].title = `STEM Discord`;
       return args;
@@ -15,7 +28,6 @@ module.exports = {
       cert: fs.readFileSync(`./local/cert.pem`)},
     public: `http${process.env.NO_HTTPS ? `` : `s`}://localhost:3000`,
   },
-  ignoredElements: [/^ion-/],
   pluginOptions: {
     express: {
       shouldServeApp: true,
