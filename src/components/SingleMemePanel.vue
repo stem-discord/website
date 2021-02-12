5c51523470f8c1d89d31b5b1c11843b69b6f7854
nopeless <nopeless@users.noreply.github.com>
2021-02-12 13:41:01 -0800

    todo: add global ion components

--- src/components/SingleMemePanel.vue
+++ src/components/SingleMemePanel.vue
@@ -1,13 +1,21 @@
 // this should be a single meme with an upvote, downvote, and report button
 <template>
   <div class="meme-panel">
-    meme
+    <ion-icon name="caret-up-outline"
+              color="red"/>
+    <ion-icon name="caret-down-outline"
+              color="blue"/>
+    <img src="https://cdn.discordapp.com/attachments/799954350477672458/809313781360230450/testing.png"
+         alt="meme image"/>
   </div>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    url: String,
+    comments: Array,
+  },
 };
 </script>
 
