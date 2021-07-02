7b61f4bfb1ac9a4e3e80b7ec67c0231d503f24dc
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-03 04:35:58 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,97 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: `module`,
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
+  plugins: [`vue`],
+  extends: [`plugin:vue/vue3-essential`, `eslint:recommended`],
+  rules: {
+    indent: [`error`, 2],
+    "linebreak-style": [`error`, `unix`],
+    quotes: [`error`, `backtick`],
+    semi: [`error`, `always`],
+    "no-multiple-empty-lines": [`error`, { max: 2, maxEOF: 0 }],
+    "quote-props": [`error`, `as-needed`],
+    strict: 0,
+    "comma-dangle": [`error`, `always-multiline`],
+    "space-infix-ops": [`error`, { int32Hint: false }],
+    "max-len": [`error`, { 
+      code: 80,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: false,
+    }],
+    "require-jsdoc": [`off`],
+    "new-cap": [`error`, { newIsCap: true}],
+    "vue/html-indent": [
+      `error`,
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: [],
+      },
+    ],
+    "vue/script-indent": [
+      `error`,
+      2,
+      {
+        baseIndent: 0,
+        switchCase: 1,
+        ignores: [],
+      },
+    ],
+    "vue/max-attributes-per-line": [
+      `error`,
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: true,
+        },
+      },
+    ],
+    "vue/html-closing-bracket-newline": [
+      `error`,
+      {
+        singleline: `never`,
+        multiline: `never`,
+      },
+    ],
+    "vue/html-self-closing": [
+      `error`,
+      {
+        html: {
+          void: `always`,
+          normal: `always`,
+          component: `always`,
+        },
+        svg: `always`,
+        math: `always`,
+      },
+    ],
+    "vue/attribute-hyphenation": [`error`, `always`],
+  },
+  overrides: [
+    {
+      files: [
+        `**/__tests__/*.{j,t}s?(x)`,
+        `**/tests/unit/**/*.spec.{j,t}s?(x)`,
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,83 @@
+# This is a rewrite branch
+
+```js
+
+const routes = [
+  {
+    path: `/`,
+    name: `Home`,
+    component: Home,
+  },
+  {
+    path: `/about`,
+    name: `About`,
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function() {
+      return import(/* webpackChunkName: "about" */ `@/views/About.vue`);
+    },
+  },{
+    path: `/sponsors`,
+    name: `sponsors`,
+    component: function() {
+      return import(`@/views/Sponsors.vue`);
+    },
+  },
+  {
+    path: `/rules`,
+    name: `rules of the server`,
+    component: () => import(`@/views/MarkdownViews/Rules.vue`),
+  },
+  {
+    path: `/tos`,
+    name: `tos of the server`,
+    component: () => import(`@/views/MarkdownViews/TOS.vue`),
+  },
+  {
+    path: `/meme`,
+    name: `we doin meme events`,
+    component: () => import(`@/views/Meme.vue`),
+  },
+  {
+    path: `/memes`,
+    name: `grid of memes`,
+    component: () => import(`@/views/Memes.vue`),
+  },
+  {
+    path: `/404`,
+    name: `explicit 404`,
+    component: function() {
+      return import(`@/views/404.vue`);
+    },
+  },
+  {
+    path: `/:pathMatch(.*)*`,
+    name: `catch page`,
+    component: function() {
+      return import(`@/views/404.vue`);
+    },
+  },
+];
+
+// https://router.vuejs.org/guide/advanced/scroll-behavior.html
+function scrollBehavior (to, from, savedPosition) {
+  if (savedPosition && to.hash !== from.hash) {
+    return savedPosition;
+  }
+  if (to.hash) {
+    const item = document.querySelector(`a.anchor[href="${to.hash}"]`);
+    if (item) {
+      item.scrollIntoView({ behavior: `smooth`});
+    }
+  }
+}
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior,
+});
+
+export default router;
+```
