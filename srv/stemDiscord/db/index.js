84a123a84ccd9f282df8a8c434f6510d3a64c32b
nopeless <38830903+nopeless@users.noreply.github.com>
2021-07-29 22:17:52 +0900

    index the schemas

--- srv/stemDiscord/db/index.js
+++ srv/stemDiscord/db/index.js
@@ -1,5 +1,9 @@
 const userInfoPublicSchema = require(`${__dirname}/schemas/userInfoPublic.js`);
+const thankedLogSchemas = require(`${__dirname}/schemas/thankedLog.js`);
+const aggregateUserInfoSchemas = require(`${__dirname}/schemas/aggregateUserInfo.js`);
 
 module.exports = {
   userInfoPublicSchema,
+  thankedLogSchemas,
+  aggregateUserInfoSchemas,
 };
